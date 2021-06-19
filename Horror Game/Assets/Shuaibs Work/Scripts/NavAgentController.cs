f6e7d155aa672292e378023cb082ca4d9a3e7388
Ahmad Dorani <Ahmad.Dorani@mds.torrens.edu.au>
2021-06-19 22:08:34 +1000

    implemented character flipping

--- Horror Game/Assets/Shuaibs Work/Scripts/NavAgentController.cs
+++ Horror Game/Assets/Shuaibs Work/Scripts/NavAgentController.cs
@@ -12,15 +12,51 @@ public class NavAgentController : MonoBehaviour
     //reference to the camera used to shoot rays
     [SerializeField] private Camera cam;
 
+    //bool to check the direction of the character
+    [SerializeField] private bool facingRight = true;
+
+    private SpriteRenderer rend;
+
+    private void Start()
+    {
+        rend = gameObject.GetComponent<SpriteRenderer>();
+    }
+
     private void Update()
     {
         //move to mouse click
         if (Input.GetMouseButtonDown(0)) 
         {
+            //checking for flipping the character sprite
+            //save the difference in vectors between the mouse click position and the players position
+            //var delta = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+
+            
+
             RaycastHit hit;
             if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
                 this.GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(hit.point);
+                var delta = hit.point - transform.position;
+
+                //Debug.Log(delta.x);
+
+                //if the mouse points at right side of the player
+                if (delta.x >= 0 && !facingRight)
+                {
+                    //transform.localScale = new Vector3(1, 1, 1); //activate looking right
+                    facingRight = true;
+                    rend.flipX = true;
+                }
+                else if (delta.x < 0 && facingRight)
+                {
+                    //mouse points to the left side of the player
+                    //transform.localScale = new Vector3(-1, 1, 1);
+                    facingRight = false;
+                    rend.flipX = false;
+                }
+
             }
         }
 
@@ -44,6 +80,7 @@ public class NavAgentController : MonoBehaviour
                 }
             }
         }
+
     }
 
     IEnumerator PlayNoise()
